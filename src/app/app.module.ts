8ca00d59988b5550e371713bc45090e527ac164f
Faber Herrera <faber.herrera@wearemo.com>
2022-08-18 14:21:34 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomRangesComponent } from './components/custom-ranges/custom-ranges.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CustomRangesComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxDaterangepickerMd.forRoot(),
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/custom-ranges/custom-ranges.component.ts
@@ -0,0 +1,80 @@
+import { Component } from '@angular/core';
+import dayjs, { Dayjs } from 'dayjs';
+import {
+  DateRange,
+  DateRanges,
+  TimePeriod,
+} from 'ngx-daterangepicker-material/daterangepicker.component';
+
+@Component({
+  selector: 'custom-ranges',
+  templateUrl: './custom-ranges.component.html',
+})
+export class CustomRangesComponent {
+  selected: TimePeriod;
+  alwaysShowCalendars: boolean;
+  showRangeLabelOnInput: boolean;
+  keepCalendarOpeningWithRange: boolean;
+  maxDate: dayjs.Dayjs;
+  minDate: dayjs.Dayjs;
+  invalidDates: dayjs.Dayjs[] = [];
+  tooltips = [
+    { date: dayjs(), text: 'Today is just unselectable' },
+    { date: dayjs().add(2, 'days'), text: 'Yeeeees!!!' },
+  ];
+
+  inlineDateTime: TimePeriod;
+  ranges: DateRanges = {
+    ['Today']: [dayjs(), dayjs()],
+    ['Yesterday']: [dayjs().subtract(1, 'days'), dayjs().subtract(1, 'days')],
+    ['Last 7 Days']: [dayjs().subtract(6, 'days'), dayjs()],
+    ['Last 30 Days']: [dayjs().subtract(29, 'days'), dayjs()],
+    ['This Month']: [dayjs().startOf('month'), dayjs().endOf('month')],
+    ['Last Month']: [
+      dayjs().subtract(1, 'month').startOf('month'),
+      dayjs().subtract(1, 'month').endOf('month'),
+    ],
+    ['Last 3 Month']: [
+      dayjs().subtract(3, 'month').startOf('month'),
+      dayjs().subtract(1, 'month').endOf('month'),
+    ],
+  };
+
+  constructor() {
+    this.maxDate = dayjs().add(2, 'weeks');
+    this.minDate = dayjs().subtract(3, 'days');
+
+    this.alwaysShowCalendars = true;
+    this.keepCalendarOpeningWithRange = true;
+    this.showRangeLabelOnInput = true;
+    this.selected = {
+      startDate: dayjs().subtract(1, 'days').set('hours', 0).set('minutes', 0),
+      endDate: dayjs().subtract(1, 'days').set('hours', 23).set('minutes', 59),
+    };
+  }
+
+  isInvalidDate = (m: dayjs.Dayjs): boolean => {
+    return this.invalidDates.some((d) => d.isSame(m, 'day'));
+  };
+
+  isTooltipDate = (m: Dayjs): string | boolean | null => {
+    const tooltip = this.tooltips.find((tt) => tt.date.isSame(m, 'day'));
+    if (tooltip) {
+      return tooltip.text;
+    } else {
+      return false;
+    }
+  };
+
+  rangeClicked(range: DateRange): void {
+    console.log('[rangeClicked] range is : ', range);
+  }
+
+  datesUpdated(range: TimePeriod): void {
+    console.log('[datesUpdated] range is : ', range);
+  }
+
+  chosenDateTime(e: TimePeriod): void {
+    this.inlineDateTime = e;
+  }
+}
